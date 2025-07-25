d27d7f36dfd320c33ea4f6cd662b005271176679
jrh1013 <jrh881013@gmail.com>
2025-07-25 21:44:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ def add_page():
 def token_page():
     return render_template("token.html")
 
+@app.route("/ping")
+def ping():
+    return "pong"
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
